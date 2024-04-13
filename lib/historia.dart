f663165cf810bac76757f9cd5a214280e48e6dbc
Retituyo <darielrestituyo@hotmail.com>
2024-04-13 14:22:44 -0400

    Pages Completed: historia miembros

--- lib/historia.dart
+++ lib/historia.dart
@@ -8,9 +8,63 @@ class HistoriaScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Historia'),
+        backgroundColor: Colors.deepPurple, // Cambia el color del AppBar
       ),
-      body: const Center(
-        child: Text('Pantalla de Historia'),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // Puedes agregar una imagen que represente la historia o la entidad
+            /*
+            Image.asset(
+              'assets/defensa_civil_history.jpg',
+              fit: BoxFit.cover,
+            ),*/
+            const SizedBox(height: 20),
+            // Título de la sección
+            const Text(
+              '¿Qué es la Defensa Civil?',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'La Defensa Civil es una organización dedicada a la protección y asistencia de '
+              'la población civil en situaciones de emergencia. Es fundamental en la respuesta '
+              'a desastres naturales, calamidades y otros eventos que pongan en riesgo la '
+              'seguridad pública.',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium
+                  ?.copyWith(fontSize: 16),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Evolución y Capacitación',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Desde su creación, la Defensa Civil ha evolucionado y se ha expandido para '
+              'incluir la educación y capacitación de la población en medidas de prevención '
+              'y respuesta ante emergencias, convirtiéndose en una pieza clave en la gestión '
+              'de riesgos y desastres en múltiples países.',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium
+                  ?.copyWith(fontSize: 16),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
--- lib/miembros.dart
+++ lib/miembros.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class MiembrosScreen extends StatelessWidget {
   const MiembrosScreen({super.key});
+
+  Future<List<dynamic>> fetchMembers() async {
+    final response = await http
+        .get(Uri.parse('https://adamix.net/defensa_civil/def/miembros.php'));
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      if (data['exito']) {
+        return data[
+            'datos']; // Usamos el campo 'datos' que contiene la lista de miembros
+      } else {
+        throw Exception('Failed to load data: ${data['mensaje']}');
+      }
+    } else {
+      throw Exception('Failed to load members');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +27,32 @@ class MiembrosScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Miembros'),
       ),
-      body: const Center(
-        child: Text('Pantalla de Miembros'),
+      body: FutureBuilder<List<dynamic>>(
+        future: fetchMembers(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                var member = snapshot.data![index];
+                return ListTile(
+                  leading: member['foto'] != null
+                      ? CircleAvatar(
+                          backgroundImage: NetworkImage(member['foto']),
+                          radius: 30.0,
+                        )
+                      : null,
+                  title: Text(member['nombre']),
+                  subtitle: Text('Cargo: ${member['cargo']}'),
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
